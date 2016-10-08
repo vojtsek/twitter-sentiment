60c20787364b900c6f9d7f7d39faa9299816e654
Petr Belohlavek <petr_belohlavek@cz.ibm.com>
2016-10-08 14:35:07 +0200

    new architectur

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import tweepy
+from tweepy import Stream
 import pickle as pck
 import logging
 import sys
 import json
+import os.path as path
+from collections import OrderedDict
+
 import twitter_config
+from stream_out_listener import StdOutListener
+
+cities = ['San Francisco', 'New York', 'Boston', 'Los Angeles', 'Dallas', 'Miami']
 
 countries = [
+    ('San Francisco',),
     ("Tirana", "Albania"),
     ("Vienna", "Austria"),
     ("Brussels", "Belgium"),
@@ -28,6 +36,7 @@ countries = [
     ("Berne", "Switzerland"),
 ]
 
+
 class CountryTweets:
 
     def __init__(self, country, capital, cid, tweets=None):
@@ -44,8 +53,8 @@ class CountryTweets:
 
 
 def poly2bb(coords):
-    latitudes = list(map(lambda x: x[0], coords))
-    longitudes = list(map(lambda x: x[1], coords))
+    longitudes = list(map(lambda x: x[0], coords))
+    latitudes= list(map(lambda x: x[1], coords))
     sw_lat = min(latitudes)
     sw_long = min(longitudes)
     ne_lat = max(latitudes)
@@ -60,20 +69,35 @@ def place2coords(place, gran="city"):
 
 
 if __name__ == '__main__':
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+    logging.getLogger("tweetpy").setLevel(logging.WARNING)
+    logging.getLogger("requests").setLevel(logging.WARNING)
+    logging.getLogger("urllib3").setLevel(logging.WARNING)
 
-    logging.basicConfig(level=logging.WARNING, format='%(asctime)s %(levelname)s %(message)s')
-    logger = logging.getLogger(__name__)
-#
+    logging.info('API authentization')
     auth = tweepy.OAuthHandler(twitter_config.CONSUMER_KEY, twitter_config.CONSUMER_SECRET)
     auth.set_access_token(twitter_config.ACCESS_TOKEN, twitter_config.ACCESS_TOKEN_SECRET)
-#
     api = tweepy.API(auth)
 
-    for country in countries:
-        try:
-            place_bb = place2coords(country[0])
-            with open("bboxes.out", "a") as f:
-                f.write("{},{}\n".format(country[0], place_bb))
-        except:
-            pass
+    # for country in countries:
+    BBOX_FILE = 'bboxes.json'
+    if path.isfile(BBOX_FILE):
+        logging.info('Using the cached bounding boxes from file %s', BBOX_FILE)
+        bboxes = json.load(open(BBOX_FILE, 'r'))
+    else:
+        logging.info('Caching the bounding boxes into file %s', BBOX_FILE)
+        bboxes = OrderedDict()
+        for city in cities:
+            try:
+                place_bb = place2coords(city)
+                bboxes[city] = place_bb
+            except:
+                print('err')
+        json.dump(bboxes, open(BBOX_FILE, 'w'))
 
+    logging.info('Creating stream')
+    stream = Stream(auth, StdOutListener())
+
+    for city, locations in bboxes.items():
+        logging.info('Tweets from %s (%s)', city, locations)
+        stream.filter(locations=locations, languages=["en"])
