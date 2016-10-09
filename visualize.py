6f51b640e3ffbefad9c85ef24b585cc64362179d
vojtsek <vojta.hudecek@gmail.com>
2016-10-09 09:24:53 +0200

    visualization prepared

--- visualize.py
+++ visualize.py
@@ -5,13 +5,13 @@ from matplotlib._png import read_png
 import pandas as pd
 import json
 
-from cartopy.io import shapereader
 from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
 
-BBOX_FILE = 'bboxes.json'
+BBOX_FILE = 'out/bboxes.json'
+
 
 def make_map(figsize=(16, 12), projection=ccrs.PlateCarree()):
-    fig = plt.figure(figsize=figsize)
+    fig = plt.figure(2, figsize=figsize)
     ax = plt.axes(projection=projection)
     gl = ax.gridlines(draw_labels=True)
     gl.xlabels_top = gl.ylabels_right = False
@@ -19,52 +19,48 @@ def make_map(figsize=(16, 12), projection=ccrs.PlateCarree()):
     gl.yformatter = LATITUDE_FORMATTER
     return fig, ax
 
-imageData = read_png('pic1.png')
-ex = (-74,40,-75,41)
-bboxes = json.load(open(BBOX_FILE, 'r'))
-data = []
-for city, locations in bboxes.items():
-    C_long = (float(locations[0]) + float(locations[2])) / 2
-    C_lat = (float(locations[1]) + float(locations[3])) / 2
-    entry = [city] + locations + [C_long, C_lat]
-    data.append(entry)
 
-data = pd.DataFrame(data, columns=['city', 'SW_long', 'SW_lat', 'NE_long', 'NE_lat', 'C_long', 'C_lat'])
-MIN_LNG = min(data['SW_long']) - 2
-MAX_LNG = max(data['NE_long']) + 2
-MIN_LAT = min(data['SW_lat']) - 2
-MAX_LAT = max(data['NE_lat']) + 2
+def create_pie(filename, data):
+    plt.pie(data, explode=(0,0,0,0), labels=None, colors=('red','pink',(0.9, 1, 0.9),'green'),
+            autopct='%1.1f%%', startangle=90,
+            radius=0.25)
+    plt.axis('equal')
+    plt.savefig(filename, transparent=True)
+    plt.clf()
 
 
-extent = [MIN_LNG, MAX_LNG, MIN_LAT, MAX_LAT]
-print(extent)
-request = cimg.OSM()
-fig, ax = make_map(figsize=(13,10), projection=request.crs)
-ax.set_extent(extent)
-ax.margins(0)
-labels=['','','','']
-explode = (0,0,0,0)
-colors = ['red','blue','green','cyan']
-ax.add_image(request, 4) # 14 je nastaveni zoom-level pro OSM mapy, cim vyssi cislo, tim vice detailu a tim dele trva stahovani
-for d in data.values:
-    print(d)
-    coords = ((d[-2] - MIN_LNG) / (MAX_LNG - MIN_LNG), (d[-1] - MIN_LAT) / (MAX_LAT - MIN_LAT))
-    print(coords)
-    # fig.figimage(imageData, fig.bbox.xmax - width, height)
-    newax = fig.add_axes([coords[0] * 0.95, coords[1] * 0.85, 0.05, 0.05], zorder=10)
-    newax.imshow(imageData)
-    newax.axis('off')
-# plt.pie(np.random.random(4), explode=explode, labels=labels, colors=colors,
-#        autopct='%1.1f%%', shadow=True, startangle=90,
-#        radius=0.25, center=(-1,0), frame=True)
-# ax.pie(np.random.random(4), explode=explode, labels=labels, colors=colors,
-#        autopct='%1.1f%%', shadow=True, startangle=90,
-#        radius=0.25, center=(1, 1), frame=True)
-# ax.pie(np.random.random(4), explode=explode, labels=labels, colors=colors,
-#        autopct='%1.1f%%', shadow=True, startangle=90,
-#        radius=0.25, center=(0, 1), frame=True)
-# ax.pie(np.random.random(4), explode=explode, labels=labels, colors=colors,
-#        autopct='%1.1f%%', shadow=True, startangle=90,
-#        radius=0.25, center=(1, 0), frame=True)
-# ax.scatter(data['C_long'], data['C_lat'], transform=ccrs.PlateCarree(), alpha=0.6, s=40, c='red')
-plt.show()
+if __name__ == '__main__':
+    bboxes = json.load(open(BBOX_FILE, 'r'))
+    data = []
+    for city, locations in bboxes.items():
+        C_long = (float(locations[0]) + float(locations[2])) / 2
+        C_lat = (float(locations[1]) + float(locations[3])) / 2
+        entry = [city] + locations + [C_long, C_lat]
+        data.append(entry)
+
+    data = pd.DataFrame(data, columns=['city', 'SW_long', 'SW_lat', 'NE_long', 'NE_lat', 'C_long', 'C_lat'])
+    for i, d in enumerate(data.values):
+        fn = 'img/pie_{}.png'
+        create_pie(fn.format(i), [20,35,35,10])
+
+    MIN_LNG = min(data['SW_long']) - 2
+    MAX_LNG = max(data['NE_long']) + 2
+    MIN_LAT = min(data['SW_lat']) - 2
+    MAX_LAT = max(data['NE_lat']) + 2
+
+
+    extent = [MIN_LNG, MAX_LNG, MIN_LAT, MAX_LAT]
+    request = cimg.OSM()
+    fig, ax = make_map(figsize=(13,10), projection=request.crs)
+    ax.set_extent(extent)
+    ax.margins(0)
+    ax.add_image(request, 1) # 14 je nastaveni zoom-level pro OSM mapy, cim vyssi cislo, tim vice detailu a tim dele trva stahovani
+    for i, d in enumerate(data.values):
+        fn = 'img/pie_{}.png'.format(i)
+        coords = ((d[-2] - MIN_LNG) / (MAX_LNG - MIN_LNG), (d[-1] - MIN_LAT) / (MAX_LAT - MIN_LAT))
+        imageData = read_png(fn)
+        newax = fig.add_axes([coords[0] * 0.9, coords[1] * 0.75, 0.1, 0.1], zorder=10)
+        newax.imshow(imageData)
+        newax.axis('off')
+
+    plt.show()
